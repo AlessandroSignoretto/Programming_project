8f3de4f5127d8397a49b8237dd27e7d617b7e1c4
alesigno99@gmail.com <alesigno99@gmail.com>
2023-08-29 22:10:01 +0200

    Small fixes

--- Programming_project_streamlit.py
+++ Programming_project_streamlit.py
@@ -13,7 +13,6 @@ from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-import folium
 
 st.title('Programming project')
 
@@ -30,6 +29,28 @@ url = 'https://www.kaggle.com/datasets/dgomonov/new-york-city-airbnb-open-data'
 st.write('The dataset I used can be found on the following link: [link to kaggle](' +url+')')
 
 NY_dataset = pd.read_csv("C:\\Users\\alesi\\Desktop\\Programming\\Programming_project\\AB_NYC_2019.csv")
+
+if st.sidebar.checkbox('Have a look at the dataset'):
+    st.write('First five rows of the dataset')
+    st.write(NY_dataset.head(5))
+    '''Before we perform any analysis, we'll first see what our dataset looks like. These are the variables it contains:
+
+    - id: - id number that identifies the property
+    - name: - Property name
+    - host_id: - id number that identifies the host
+    - host_name: - Host name
+    - neighbourhood_group: - The main regions of the city
+    - neighbourhood: - The neighbourhoods
+    - latitude: - Property latitude
+    - longitude: - Property longitude
+    - room_type: - Type of the room
+    - price: - The price for one night
+    - minimum_nights: - Minimum amount of nights to book the place
+    - number_of_reviews: - Number of reviews received
+    - last_review: - Date of the last review
+    - reviews_per_month: - Amount of reviews per month
+    - calculated_host_listings_count: - Number of properties available on Airbnb owned by the host
+    - availability_365: - Number of days of availability within 365 days'''
 
 #Data cleaning
 NY_dataset.drop(['name', 'host_name', 'last_review'], axis=1, inplace=True)
@@ -53,6 +74,18 @@ z_scores = np.abs(stats.zscore(NY_dataset_col))
 NY_dataset_wo = NY_dataset[(z_scores < 2).all(axis=1)]
 NY_dataset_wo.shape
 
+
+#Correlation matrix
+st.write('If we would like too see how much the variables are correlated, we should plot the correlation matrix:')
+corr = NY_dataset_wo[['price', 'minimum_nights', 'number_of_reviews','reviews_per_month', 'calculated_host_listings_count', 'availability_365']].corr()
+plt.figure(figsize=(5,4))
+heatmap = sns.heatmap(corr, cmap='RdBu', fmt='.3f', annot=True)
+heatmap.set_xticklabels(heatmap.get_xticklabels(), fontsize=8)
+heatmap.set_yticklabels(heatmap.get_yticklabels(), fontsize=8)
+plt.xticks(rotation=35, ha='right')
+plt.show()
+st.pyplot(plt)
+
 #Analysis
 #Regression analysis to predict the price
 NY_final = NY_dataset_wo.drop(['id','host_id','latitude','longitude','neighbourhood'], axis = 1, inplace = False)
@@ -71,10 +104,6 @@ X_train, X_test, y_train, y_test =  train_test_split(X,y,test_size = 0.2, random
 
 
 #GRAFICO NEW YORK
-if st.sidebar.checkbox('Have a look at the dataset'):
-    st.write('First five rows of the dataset')
-    st.write(NY_dataset.head(5))
-
 # Lista dei room_type disponibili nel tuo dataset
 available_room_types = NY_dataset['room_type'].unique()
 
@@ -113,68 +142,68 @@ X_train_low, X_test_low, y_train_low, y_test_low =  train_test_split(X_low, y_lo
 
 with st.expander('Models to predict the price'):
 
-    st.subheader('R^2, RMSE and plot error')
+    st.subheader('$R^{2}$, $RMSE$ $and$ $plot$ $error$')
 
     select_price = st.selectbox('Select price:', ['All', 'Price under $249'])
     if select_price == 'All':
+        with st.spinner('Training...'):
+            select_model = st.selectbox('Select model:', ['Linear Regression','Ridge','Regression Tree','Random Forest', 'Gradient Boosting'])
+            
+            if select_model == 'Linear Regression':
+                model = LinearRegression()
+            elif select_model == 'Ridge':
+                model = Ridge(alpha=0.01)
+            elif select_model == 'Regression Tree':  
+                model = DecisionTreeRegressor(min_samples_leaf=.01)
+            elif select_model == 'Random Forest':
+                model = RandomForestRegressor(n_estimators=200, max_depth = 45, min_samples_leaf = 20)
+            elif select_model == 'Gradient Boosting':  
+                model = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.01)
+            
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
 
-        select_model = st.selectbox('Select model:', ['Linear Regression','Ridge','Regression Tree','Random Forest', 'Gradient Boosting'])
-        
-        if select_model == 'Linear Regression':
-            model = LinearRegression()
-        elif select_model == 'Ridge':
-            model = Ridge(alpha=0.01)
-        elif select_model == 'Regression Tree':  
-            model = DecisionTreeRegressor(min_samples_leaf=.01)
-        elif select_model == 'Random Forest':
-            model = RandomForestRegressor(n_estimators=200, max_depth = 45, min_samples_leaf = 20)
-        elif select_model == 'Gradient Boosting':  
-            model = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.01)
-        
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
+            st.write("R2 score: ", r2_score(y_test, y_pred))
+            st.write("RMSE: ", np.sqrt(mean_squared_error(y_test, y_pred)))
 
-        st.write("R2 score: ", r2_score(y_test, y_pred))
-        st.write("RMSE: ", np.sqrt(mean_squared_error(y_test, y_pred)))
+            error_diff = pd.DataFrame({'Actual Values': np.array(y_test).flatten(), 'Predicted Values': y_pred.flatten()})
 
-        error_diff = pd.DataFrame({'Actual Values': np.array(y_test).flatten(), 'Predicted Values': y_pred.flatten()})
-
-        df1 = error_diff.head(20)
-        df1.plot(kind='bar', figsize=(7, 4))
-        plt.grid(linewidth='0.5', color='black')
-        st.pyplot(plt)
-        pass
+            df1 = error_diff.head(20)
+            df1.plot(kind='bar', figsize=(7, 4))
+            plt.grid(linewidth='0.5', color='black')
+            st.pyplot(plt)
+            pass
 
     elif select_price == 'Price under $249':
+        with st.spinner('Training...'):
+            select_model = st.selectbox('Select model:', ['Linear Regression','Ridge','Regression Tree','Random Forest', 'Gradient Boosting'])
+            
+            if select_model == 'Linear Regression':
+                model = LinearRegression()
+            elif select_model == 'Ridge':
+                model = Ridge(alpha=0.01)
+            elif select_model == 'Regression Tree':  
+                model = DecisionTreeRegressor(min_samples_leaf=.01)
+            elif select_model == 'Random Forest':
+                model = RandomForestRegressor(n_estimators=200, max_depth = 45, min_samples_leaf = 20)
+            elif select_model == 'Gradient Boosting':  
+                model = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.01)
+            
+            model.fit(X_train_low,y_train_low)
+            y_pred_low=model.predict(X_test_low)
 
-        select_model = st.selectbox('Select model:', ['Linear Regression','Ridge','Regression Tree','Random Forest', 'Gradient Boosting'])
-        
-        if select_model == 'Linear Regression':
-            model = LinearRegression()
-        elif select_model == 'Ridge':
-            model = Ridge(alpha=0.01)
-        elif select_model == 'Regression Tree':  
-            model = DecisionTreeRegressor(min_samples_leaf=.01)
-        elif select_model == 'Random Forest':
-            model = RandomForestRegressor(n_estimators=200, max_depth = 45, min_samples_leaf = 20)
-        elif select_model == 'Gradient Boosting':  
-            model = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.01)
-        
-        model.fit(X_train_low,y_train_low)
-        y_pred_low=model.predict(X_test_low)
+            #R2 score
+            st.write("R2 score: ",r2_score(y_test_low,y_pred_low))
+            st.write("RMSE: ",np.sqrt(mean_squared_error(y_test_low,y_pred_low)))
 
-        #R2 score
-        st.write("R2 score: ",r2_score(y_test_low,y_pred_low))
-        st.write("RMSE: ",np.sqrt(mean_squared_error(y_test_low,y_pred_low)))
+            #Error
+            error_diff = pd.DataFrame({'Actual Values': np.array(y_test_low).flatten(), 'Predicted Values': y_pred_low.flatten()})
 
-        #Error
-        error_diff = pd.DataFrame({'Actual Values': np.array(y_test_low).flatten(), 'Predicted Values': y_pred_low.flatten()})
-
-        #Visualize
-        df1 = error_diff.head(20)
-        df1.plot(kind='bar',figsize=(7,4))
-        plt.grid(linewidth = '0.5', color = 'black')
-        st.pyplot(plt)
+            #Visualize
+            df1 = error_diff.head(20)
+            df1.plot(kind='bar',figsize=(7,4))
+            plt.grid(linewidth = '0.5', color = 'black')
+            st.pyplot(plt)
         
 
 
